bd7f3c197d5b669a9c4cf6e16848e52c6930a3e3
Alexander Petrovski <alexanderpetrovski@Alexanders-MBP.home>
2023-11-15 18:56:13 -0500

    build 4

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/gastrograph/",
+  // root: 'root',
+  // base: "/gastrograph/",
   plugins: [react()],
   server: {
     port: 3001,
