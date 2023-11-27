85db2979f7087ce5fcf6b54fbddfe7c89db865b5
Alexander Petrovski <alexanderpetrovski@Alexanders-MacBook-Pro.local>
2023-11-27 17:12:23 -0500

    correct file path

--- src/components/NewPassword.jsx
+++ src/components/NewPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import '../styles/NewPassword.css'; 
+import '../styles/newPassword.css'; 
 
 const NewPassword = () => {
   const [newPassword, setNewPassword] = useState('');
