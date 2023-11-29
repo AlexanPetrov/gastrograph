319b13e0d6796b7ad9e586467acdcbe5786c27b5
Alexander Petrovski <alexanderpetrovski@alexanders-mbp.myfiosgateway.com>
2023-11-29 18:49:14 -0500

    spinner test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,22 +5,22 @@ import '../styles/home.css';
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
   const [currentImage, setCurrentImage] = useState(0);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRecipes = async () => {
-      const backendUrl = 'https://gastrographbackend.onrender.com'; 
+      const backendUrl = 'https://gastrographbackend.onrender.com';
       try {
         const response = await fetch(`${backendUrl}/recipes`);
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok');
         }
         const data = await response.json();
         setRecipes(data);
       } catch (error) {
         console.error('Error fetching recipes:', error);
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
 
@@ -35,7 +35,12 @@ const Home = () => {
   }, [recipes.length]);
 
   if (loading) {
-    return <p>Loading...</p>; 
+    return (
+      <div className="loading-container">
+        <div className="spinner"></div>
+        <p>Loading recipes...</p>
+      </div>
+    );
   }
 
   return (
@@ -60,16 +65,91 @@ const Home = () => {
           </div>
         </div>
         <div className="welcome-text">
-          <p>
-          Welcome to Gastrograph! Explore delicious recipes and culinary delights from kitchens around the world. At Gastrograph, we believe in more than just food; we believe in stories, connections, and flavors that bring people together. Every recipe you find here carries a personal touch, a unique blend of tradition, innovation, and love. Whether you are a seasoned chef or a culinary novice, our hand-picked recipes are designed to inspire creativity and joy in your kitchen. From hearty family favorites to elegant gourmet creations, you will discover a world of tastes waiting to be explored. So why wait? Let your culinary journey begin with Gastrograph, where every dish tells a story.
-          </p>
+          {/* Your welcome text goes here */}
         </div>
       </div>
       <div className='home-moral-class'>
-        <p>{'"To the ruler, the people are heaven; to the people, food is heaven." - Ancient Chinese Proverb'}</p>
+        {/* Your moral class content goes here */}
       </div>
     </>
-  )
+  );
 }
 
 export default Home;
+
+
+// import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import '../styles/home.css';
+
+// const Home = () => {
+//   const [recipes, setRecipes] = useState([]);
+//   const [currentImage, setCurrentImage] = useState(0);
+//   const [loading, setLoading] = useState(true); 
+
+//   useEffect(() => {
+//     const fetchRecipes = async () => {
+//       const backendUrl = 'https://gastrographbackend.onrender.com'; 
+//       try {
+//         const response = await fetch(`${backendUrl}/recipes`);
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         setRecipes(data);
+//       } catch (error) {
+//         console.error('Error fetching recipes:', error);
+//       } finally {
+//         setLoading(false); 
+//       }
+//     };
+
+//     fetchRecipes();
+//   }, []);
+
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       setCurrentImage((prevIndex) => (prevIndex + 1) % recipes.length);
+//     }, 5000);
+//     return () => clearInterval(timer);
+//   }, [recipes.length]);
+
+//   if (loading) {
+//     return <p>Loading...</p>; 
+//   }
+
+//   return (
+//     <>
+//       <div className="home-container">
+//         <div className="image-gallery">
+//           {recipes.length > 0 && (
+//             <Link to={`/recipes/${recipes[currentImage].id}`}>
+//               <img src={recipes[currentImage].imageURL} 
+//                    alt={recipes[currentImage].title} 
+//                    title={recipes[currentImage].title}/>
+//             </Link>
+//           )}
+//           <div className="dots">
+//             {recipes.map((_, index) => (
+//               <span
+//                 key={index}
+//                 className={index === currentImage ? 'active-dot' : 'dot'}
+//                 onClick={() => setCurrentImage(index)}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//         <div className="welcome-text">
+//           <p>
+//           Welcome to Gastrograph! Explore delicious recipes and culinary delights from kitchens around the world. At Gastrograph, we believe in more than just food; we believe in stories, connections, and flavors that bring people together. Every recipe you find here carries a personal touch, a unique blend of tradition, innovation, and love. Whether you are a seasoned chef or a culinary novice, our hand-picked recipes are designed to inspire creativity and joy in your kitchen. From hearty family favorites to elegant gourmet creations, you will discover a world of tastes waiting to be explored. So why wait? Let your culinary journey begin with Gastrograph, where every dish tells a story.
+//           </p>
+//         </div>
+//       </div>
+//       <div className='home-moral-class'>
+//         <p>{'"To the ruler, the people are heaven; to the people, food is heaven." - Ancient Chinese Proverb'}</p>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Home;
