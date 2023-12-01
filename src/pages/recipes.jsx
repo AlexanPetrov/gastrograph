86eb4f5dca6459689df526fdb7f5e3efcd7faee0
Alexander Petrovski <alexanderpetrovski@Alexanders-MacBook-Pro.local>
2023-11-30 22:56:30 -0500

    live img loading tests 17

--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -1,32 +1,22 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import NotFound from './notfound.jsx';
 import Button from '../components/Button.jsx';
 import RecipeCard from '../components/RecipeCard.jsx';
-import '../styles/recipes.css';
-import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
 import RecipeUploadForm from '../components/RecipeUploadForm.jsx';
-
+import PropTypes from 'prop-types';
+import '../styles/recipes.css';
 
 const Recipes = ({ isLoggedIn, onLogout }) => {
   const [recipes, setRecipes] = useState([]);
   const [recipesPerPage, setRecipesPerPage] = useState(3);
   const [loading, setLoading] = useState(true);
   const { recipeId } = useParams();
-
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    onLogout(); 
-    navigate('/'); 
-  };
-
-
   useEffect(() => {
-      const backendUrl = 'https://gastrographbackend.onrender.com'; 
-
-  fetch(`${backendUrl}/recipes`, {
+    const backendUrl = 'https://gastrographbackend.onrender.com';
+    fetch(`${backendUrl}/recipes`, {
       credentials: 'include',
       method: 'GET',
     })
@@ -38,110 +28,89 @@ const Recipes = ({ isLoggedIn, onLogout }) => {
         }
       })
       .then(data => {
-        console.log("Fetched Data: ", data); 
         setRecipes(data);
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error.message);
-        console.error("Stack trace:", error.stack);
         setLoading(false);
       });
-    
   }, [recipesPerPage]);
-  
 
-  const displayedRecipes = recipes.slice(0, recipesPerPage);
+  const handleLogout = () => {
+    onLogout();
+    navigate('/');
+  };
 
   const handleLoadMore = () => {
     setRecipesPerPage(prev => prev + 3);
   };
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  const selectedRecipe = recipes.find((r) => r.id === recipeId);
-
-  if (recipeId && selectedRecipe) {
-    return (
-      <div className="expanded-recipe-card">
-        <img className="expanded-recipe-card-img" src={selectedRecipe.imageURL} alt={selectedRecipe.title} />
-        <div className="expanded-recipe-card-details">
-          <h1>{selectedRecipe.title}</h1>
-          <h3>Ingredients:</h3>
-          <ul>
-            {selectedRecipe.ingredients.map((ingredient, index) => (
-              <li key={index}>{ingredient}</li>
-            ))}
-          </ul>
-          <h3>Instructions:</h3>
-          <ul>
-            {selectedRecipe.instructions.map((instruction, index) => (
-              <li key={index}>{instruction}</li>
-            ))}
-          </ul>
-          <h3>Author:</h3>
-          <p>{selectedRecipe.author}</p>
-          <h3>Date Shared:</h3>
-          <p>{new Date(selectedRecipe.date).toLocaleDateString()}</p>
-        </div>
-      </div>
-    );
-  } else if (recipeId && !selectedRecipe) {
-    return <NotFound />;
-  }
+  const handleRecipeSubmit = (newRecipe) => {
+    const backendUrl = 'https://gastrographbackend.onrender.com';
+    fetch(`${backendUrl}/recipes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newRecipe),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(addedRecipe => {
+        setRecipes(prevRecipes => [...prevRecipes, addedRecipe]);
+      })
+      .catch(error => {
+        console.error("Error submitting new recipe:", error.message);
+      });
+  };
 
   const logoutButton = isLoggedIn ? (
     <div className="logout-button-container">
       <Button text="Log Out" onClick={handleLogout} className="button" />
     </div>
-  ) : null; 
-  
-  const handleRecipeSubmit = (newRecipe) => {
+  ) : null;
 
-    const backendUrl = 'https://gastrographbackend.onrender.com'; 
+  const selectedRecipe = recipes.find((r) => r.id === recipeId);
 
-fetch(`${backendUrl}/recipes`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newRecipe),
-})
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then(addedRecipe => {
-      setRecipes(prevRecipes => [...prevRecipes, addedRecipe]);
-    })
-    .catch(error => {
-      console.error("Error submitting new recipe:", error.message);
-    });
-  };
+  if (recipeId && !selectedRecipe) {
+    return <NotFound />;
+  }
+
+  const displayedRecipes = loading ? (
+    <p>Loading...</p>
+  ) : (
+    recipes.slice(0, recipesPerPage).map((recipe) => (
+      <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
+        <RecipeCard recipe={recipe} />
+      </Link>
+    ))
+  );
 
   return (
     <>
       <div className='logoutBtn'>{logoutButton}</div>
-      {isLoggedIn && (
-      <>
-        <RecipeUploadForm onRecipeSubmit={handleRecipeSubmit} />
-      </>
-    )}
+      {isLoggedIn && <RecipeUploadForm onRecipeSubmit={handleRecipeSubmit} />}
       <div className="recipe-page">
-        <div className="recipe-page-wrapper">
-          {displayedRecipes.map((recipe) => (
-            <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
-              <RecipeCard recipe={recipe} />
-            </Link>
-          ))}
-        </div>
-      </div>
-      <div className="centered-button">
-        <Button text="See More Recipes" onClick={handleLoadMore} className="button" />
+        {recipeId && selectedRecipe ? (
+          // JSX to display the selected recipe's details
+          <div>
+            {/* Insert JSX for displaying the selected recipe here */}
+          </div>
+        ) : (
+          <>
+            <div className="recipe-page-wrapper">
+              {displayedRecipes}
+            </div>
+            <div className="centered-button">
+              <Button text="See More Recipes" onClick={handleLoadMore} className="button" />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
@@ -153,3 +122,160 @@ Recipes.propTypes = {
 };
 
 export default Recipes;
+
+
+// import { useState, useEffect } from 'react';
+// import { useParams, Link } from 'react-router-dom';
+// import NotFound from './notfound.jsx';
+// import Button from '../components/Button.jsx';
+// import RecipeCard from '../components/RecipeCard.jsx';
+// import '../styles/recipes.css';
+// import PropTypes from 'prop-types';
+// import { useNavigate } from 'react-router-dom';
+// import RecipeUploadForm from '../components/RecipeUploadForm.jsx';
+
+
+// const Recipes = ({ isLoggedIn, onLogout }) => {
+//   const [recipes, setRecipes] = useState([]);
+//   const [recipesPerPage, setRecipesPerPage] = useState(3);
+//   const [loading, setLoading] = useState(true);
+//   const { recipeId } = useParams();
+
+//   const navigate = useNavigate();
+
+//   const handleLogout = () => {
+//     onLogout(); 
+//     navigate('/'); 
+//   };
+
+
+//   useEffect(() => {
+//       const backendUrl = 'https://gastrographbackend.onrender.com'; 
+
+//   fetch(`${backendUrl}/recipes`, {
+//       credentials: 'include',
+//       method: 'GET',
+//     })
+//       .then(response => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
+//         }
+//       })
+//       .then(data => {
+//         console.log("Fetched Data: ", data); 
+//         setRecipes(data);
+//         setLoading(false);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error.message);
+//         console.error("Stack trace:", error.stack);
+//         setLoading(false);
+//       });
+    
+//   }, [recipesPerPage]);
+  
+
+//   const displayedRecipes = recipes.slice(0, recipesPerPage);
+
+//   const handleLoadMore = () => {
+//     setRecipesPerPage(prev => prev + 3);
+//   };
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   const selectedRecipe = recipes.find((r) => r.id === recipeId);
+
+//   if (recipeId && selectedRecipe) {
+//     return (
+//       <div className="expanded-recipe-card">
+//         <img className="expanded-recipe-card-img" src={selectedRecipe.imageURL} alt={selectedRecipe.title} />
+//         <div className="expanded-recipe-card-details">
+//           <h1>{selectedRecipe.title}</h1>
+//           <h3>Ingredients:</h3>
+//           <ul>
+//             {selectedRecipe.ingredients.map((ingredient, index) => (
+//               <li key={index}>{ingredient}</li>
+//             ))}
+//           </ul>
+//           <h3>Instructions:</h3>
+//           <ul>
+//             {selectedRecipe.instructions.map((instruction, index) => (
+//               <li key={index}>{instruction}</li>
+//             ))}
+//           </ul>
+//           <h3>Author:</h3>
+//           <p>{selectedRecipe.author}</p>
+//           <h3>Date Shared:</h3>
+//           <p>{new Date(selectedRecipe.date).toLocaleDateString()}</p>
+//         </div>
+//       </div>
+//     );
+//   } else if (recipeId && !selectedRecipe) {
+//     return <NotFound />;
+//   }
+
+//   const logoutButton = isLoggedIn ? (
+//     <div className="logout-button-container">
+//       <Button text="Log Out" onClick={handleLogout} className="button" />
+//     </div>
+//   ) : null; 
+  
+//   const handleRecipeSubmit = (newRecipe) => {
+
+//     const backendUrl = 'https://gastrographbackend.onrender.com'; 
+
+// fetch(`${backendUrl}/recipes`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newRecipe),
+// })
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
+//       }
+//       return response.json();
+//     })
+//     .then(addedRecipe => {
+//       setRecipes(prevRecipes => [...prevRecipes, addedRecipe]);
+//     })
+//     .catch(error => {
+//       console.error("Error submitting new recipe:", error.message);
+//     });
+//   };
+
+//   return (
+//     <>
+//       <div className='logoutBtn'>{logoutButton}</div>
+//       {isLoggedIn && (
+//       <>
+//         <RecipeUploadForm onRecipeSubmit={handleRecipeSubmit} />
+//       </>
+//     )}
+//       <div className="recipe-page">
+//         <div className="recipe-page-wrapper">
+//           {displayedRecipes.map((recipe) => (
+//             <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
+//               <RecipeCard recipe={recipe} />
+//             </Link>
+//           ))}
+//         </div>
+//       </div>
+//       <div className="centered-button">
+//         <Button text="See More Recipes" onClick={handleLoadMore} className="button" />
+//       </div>
+//     </>
+//   );
+// };
+
+// Recipes.propTypes = {
+//   isLoggedIn: PropTypes.bool.isRequired,
+//   onLogout: PropTypes.func.isRequired,
+// };
+
+// export default Recipes;
