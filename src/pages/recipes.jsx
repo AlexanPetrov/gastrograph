1a996ee624f8e65e842eeeac96cb088eae53ac07
Alexander Petrovski <alexanderpetrovski@Alexanders-MacBook-Pro.local>
2023-11-30 23:28:01 -0500

    live img loading tests 21

--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -15,39 +15,41 @@ const Recipes = ({ isLoggedIn, onLogout }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const backendUrl = 'https://gastrographbackend.onrender.com';
+    const backendUrl = 'https://gastrographbackend.onrender.com'; 
     fetch(`${backendUrl}/recipes`, {
-      credentials: 'include',
-      method: 'GET',
+        credentials: 'include',
+        method: 'GET',
     })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
-        }
-      })
-      .then(data => {
-        setRecipes(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error.message);
-        setLoading(false);
-      });
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
+      }
+    })
+    .then(data => {
+      setRecipes(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error.message);
+      setLoading(false);
+    });
   }, [recipesPerPage]);
 
   const handleLogout = () => {
-    onLogout();
-    navigate('/');
+    onLogout(); 
+    navigate('/'); 
   };
+
+  const displayedRecipes = recipes.slice(0, recipesPerPage);
 
   const handleLoadMore = () => {
     setRecipesPerPage(prev => prev + 3);
   };
 
   const handleRecipeSubmit = (newRecipe) => {
-    const backendUrl = 'https://gastrographbackend.onrender.com';
+    const backendUrl = 'https://gastrographbackend.onrender.com'; 
     fetch(`${backendUrl}/recipes`, {
       method: 'POST',
       headers: {
@@ -55,19 +57,54 @@ const Recipes = ({ isLoggedIn, onLogout }) => {
       },
       body: JSON.stringify(newRecipe),
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(addedRecipe => {
-        setRecipes(prevRecipes => [...prevRecipes, addedRecipe]);
-      })
-      .catch(error => {
-        console.error("Error submitting new recipe:", error.message);
-      });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(addedRecipe => {
+      setRecipes(prevRecipes => [...prevRecipes, addedRecipe]);
+    })
+    .catch(error => {
+      console.error("Error submitting new recipe:", error.message);
+    });
   };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const selectedRecipe = recipes.find((r) => r.id === recipeId);
+
+  if (recipeId && selectedRecipe) {
+    return (
+      <div className="expanded-recipe-card">
+        <img className="expanded-recipe-card-img" src={selectedRecipe.imageURL} alt={selectedRecipe.title} />
+        <div className="expanded-recipe-card-details">
+          <h1>{selectedRecipe.title}</h1>
+          <h3>Ingredients:</h3>
+          <ul>
+            {selectedRecipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+          <h3>Instructions:</h3>
+          <ul>
+            {selectedRecipe.instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ul>
+          <h3>Author:</h3>
+          <p>{selectedRecipe.author}</p>
+          <h3>Date Shared:</h3>
+          <p>{new Date(selectedRecipe.date).toLocaleDateString()}</p>
+        </div>
+      </div>
+    );
+  } else if (recipeId && !selectedRecipe) {
+    return <NotFound />;
+  }
 
   const logoutButton = isLoggedIn ? (
     <div className="logout-button-container">
@@ -75,42 +112,23 @@ const Recipes = ({ isLoggedIn, onLogout }) => {
     </div>
   ) : null;
 
-  const selectedRecipe = recipes.find((r) => r.id === recipeId);
-
-  if (recipeId && !selectedRecipe) {
-    return <NotFound />;
-  }
-
-  const displayedRecipes = loading ? (
-    <p>Loading...</p>
-  ) : (
-    recipes.slice(0, recipesPerPage).map((recipe) => (
-      <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
-        <RecipeCard recipe={recipe} />
-      </Link>
-    ))
-  );
-
   return (
     <>
       <div className='logoutBtn'>{logoutButton}</div>
-      {isLoggedIn && <RecipeUploadForm onRecipeSubmit={handleRecipeSubmit} />}
+      {isLoggedIn && (
+        <RecipeUploadForm onRecipeSubmit={handleRecipeSubmit} />
+      )}
       <div className="recipe-page">
-        {recipeId && selectedRecipe ? (
-          // JSX to display the selected recipe's details
-          <div>
-            {/* Insert JSX for displaying the selected recipe here */}
-          </div>
-        ) : (
-          <>
-            <div className="recipe-page-wrapper">
-              {displayedRecipes}
-            </div>
-            <div className="centered-button-container">
-              <Button text="See More Recipes" onClick={handleLoadMore} className="centered-button" />
-            </div>
-          </>
-        )}
+        <div className="recipe-page-wrapper">
+          {displayedRecipes.map((recipe) => (
+            <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
+              <RecipeCard recipe={recipe} />
+            </Link>
+          ))}
+        </div>
+        <div className="centered-button-container">
+          <Button text="See More Recipes" onClick={handleLoadMore} className="centered-button" />
+        </div>
       </div>
     </>
   );
@@ -122,6 +140,7 @@ Recipes.propTypes = {
 };
 
 export default Recipes;
+
 
 
 
